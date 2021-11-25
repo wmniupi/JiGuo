2174a839e126b2a19763af8aecc3109225c45541
Ares <1390566919@qq.com>
2021-11-25 15:49:40 +0800

    'header

--- js/index.js
+++ js/index.js
@@ -1,17 +1,13 @@
 window.addEventListener('load', function () {
-    let registerbtn = document.querySelector('.registerbtn');
+    /* let registerbtn = document.querySelector('.registerbtn'); */
     /* 注册按钮 鼠标悬浮样式改变  */
-    registerbtn.addEventListener('mouseenter', function () {
-        this.style.backgroundColor = '#fe5341';
-        this.style.color = 'white';
-    });
-    registerbtn.addEventListener('mouseleave', function () {
-        this.style.backgroundColor = 'white';
-        this.style.color = '#fe5341';
-    })
+    /*  registerbtn.addEventListener('mouseenter', function () {
+         this.style.backgroundColor = '#fe5341';
+         this.style.color = 'white';
+     });
+     registerbtn.addEventListener('mouseleave', function () {
+         this.style.backgroundColor = 'white';
+         this.style.color = '#fe5341';
+     }) */
     /* 导航栏ul鼠标悬浮样式变化 */
-
-
-
-
 })
