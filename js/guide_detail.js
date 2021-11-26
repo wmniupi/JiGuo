84b033752ce1ef4450cdf3951854b3f435662b37
江冰封 <2944279298@qq.com>
2021-11-26 16:59:58 +0800

    '页面跳转js实现'

--- js/guide_detail.js
+++ js/guide_detail.js
@@ -9,13 +9,13 @@ window.addEventListener('load',function(){
     like.addEventListener('click',function(){
         if(bool1){
             bool1 = false;
-            this.children[0].src='../../img/xinRedh.png';
+            this.children[0].src='../img/xinRedh.png';
             this.style.color = '#fe5431'
             // console.log(this.childNodes[3].childNodes[0].nodeValue);
             this.children[2].innerHTML = parseInt(this.childNodes[3].childNodes[0].nodeValue)+1;
         }else{
             bool1 = true;
-            this.children[0].src='../../img/xinRedo.png';
+            this.children[0].src='../img/xinRedo.png';
             this.style.color = '#ababab'
             // console.log(this.childNodes[3].childNodes[0].nodeValue);
             this.children[2].innerHTML = parseInt(this.childNodes[3].childNodes[0].nodeValue)-1;
@@ -23,7 +23,7 @@ window.addEventListener('load',function(){
         
     })
     share.addEventListener('click',function(){
-        this.children[0].src='../../img/shareh_yj1.png';
+        this.children[0].src='../img/shareh_yj1.png';
         this.style.color = '#fe5431'
         this.childNodes[2].nodeValue = '已分享';        
     })
