96d427264d7fe35b804fdd39e37e612c29b484eb
‘王萌’ <1335665801@qq.com>
2021-11-26 19:38:25 +0800

    '最后一次'

--- js/user02.js
+++ js/user02.js
@@ -1,13 +1,13 @@
 window.onload =function(){
     var span = document.getElementsByTagName('span');
 span[0].addEventListener('click',()=>{
-    window.location.href = '../user/tryout.html'
+    window.location.href = './tryout.html'
 })
  span[1].addEventListener('click',()=>{
-        window.location.href = '../user/user02.html'
+        window.location.href = './user02.html'
     })
 span[2].addEventListener('click',()=>{
-    window.location.href = '../user/tryout.html'
+    window.location.href = './tryout.html'
 })
 span[3].addEventListener('click',()=>{
     window.location.href = './user03.html'
@@ -18,4 +18,10 @@ span[4].addEventListener('click',()=>{
 span[5].addEventListener('click',()=>{
     window.location.href = './user05.html'
 })
+var li = document.getElementsByTagName('li');
+for(var i=0;i<li.length;i++){
+    li[i].onclick = function(){
+        window.location.href = './playskip.html'
+    }
+}
 }
