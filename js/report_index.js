250e60e815071d030df1fd8c422531ff15134dcd
江冰封 <2944279298@qq.com>
2021-11-26 14:24:31 +0800

    'jbf的更新：对report_index加载更多的更新'

--- js/report_index.js
+++ js/report_index.js
@@ -6,9 +6,9 @@
     // 加载更多实现效果
     const more_ = document.querySelector('#more');
     // console.log(more_);
-    more_.children[0].addEventListener('click',function(){
-        this.innerHTML = '<img src="../../img/loading-icon.gif"/>&nbsp;&nbsp;正在加载中';
-    });
+    // more_.children[0].addEventListener('click',function(){
+    //     this.innerHTML = '<img src="../../img/loading-icon.gif"/>&nbsp;&nbsp;正在加载中';
+    // });
     // 最新和最热的切换
     const btn_new = document.getElementById('btn_new');
     const btn_hot = document.getElementById('btn_hot');
@@ -47,17 +47,27 @@
     // 渲染数据
     function showdata(v){
         var str = '';
-        for(var i of v){
-            // console.log(i);
+        // for(var i of v){
+        //     // console.log(i);
+        //     str += '<li>';
+        //     str += '<div><img src="'+i.img+'"/>';
+        //     str += '<div>'+i.text;
+        //     str += '<span><img src="../../img/tx.jpg" alt="">'+i.endTime+'</span>';
+        //     str += '<span><img src="../../img/zan.png" alt="">'+i.totalnum;
+        //     str += '<img src="../../img/reply.png" alt="">'+i.apply+'</span>';
+        //     str += '</div></div>';
+        //     str += '<div>关于此产品还有'+i.num+'篇报告，点击查看</div>'; 
+        // }
+        var bool = true;
+        for (let i = 0; i<5; i++){
             str += '<li>';
-            str += '<div><img src="'+i.img+'"/>';
-            str += '<div>'+i.text;
-            str += '<span><img src="../../img/tx.jpg" alt="">'+i.endTime+'</span>';
-            str += '<span><img src="../../img/zan.png" alt="">'+i.totalnum;
-            str += '<img src="../../img/reply.png" alt="">'+i.apply+'</span>';
+            str += '<div><img src="'+v[i].img+'"/>';
+            str += '<div>'+v[i].text;
+            str += '<span><img src="../../img/tx.jpg" alt="">'+v[i].endTime+'</span>';
+            str += '<span><img src="../../img/zan.png" alt="">'+v[i].totalnum;
+            str += '<img src="../../img/reply.png" alt="">'+v[i].apply+'</span>';
             str += '</div></div>';
-            str += '<div>关于此产品还有'+i.num+'篇报告，点击查看</div>';
-            
+            str += '<div>关于此产品还有'+v[i].num+'篇报告，点击查看</div>'; 
         }
         const ul =  document.getElementById('show_ul');
         ul.innerHTML = str;
@@ -69,6 +79,39 @@
                 window.location.href = '../guide/guide_detail.html';
             })
         }
+        more_.children[0].innerHTML = '<img src="../../img/more.png"/>&nbsp;&nbsp;点击加载更多';
+        more_.children[0].addEventListener('click',function(){
+            if(bool){
+                bool=false;
+                this.innerHTML = '<img src="../../img/loading-icon.gif"/>&nbsp;&nbsp;正在加载中';
+                clearTimeout(timer);
+                var timer = setTimeout(function(){
+                    more_.children[0].innerHTML = '到底了~~';
+                    for (let i = 5; i<v.length; i++){
+                        str += '<li>';
+                        str += '<div><img src="'+v[i].img+'"/>';
+                        str += '<div>'+v[i].text;
+                        str += '<span><img src="../../img/tx.jpg" alt="">'+v[i].endTime+'</span>';
+                        str += '<span><img src="../../img/zan.png" alt="">'+v[i].totalnum;
+                        str += '<img src="../../img/reply.png" alt="">'+v[i].apply+'</span>';
+                        str += '</div></div>';
+                        str += '<div>关于此产品还有'+v[i].num+'篇报告，点击查看</div>'; 
+                    }
+                    const ul =  document.getElementById('show_ul');
+                    ul.innerHTML = str;
+                    // console.log(ul.children);
+                    // 给每个li增加跳转
+                    for(var li of ul.children){
+                        // console.log(li);
+                        li.addEventListener('click',function(){
+                            window.location.href = '../guide/guide_detail.html';
+                        })
+                    }
+                },1500)
+            }else{
+
+            }
+        });
 
     }
     btn_new.click();
