6ecc192ed9c500651cd33b917b69435726316214
江冰封 <2944279298@qq.com>
2021-11-27 11:27:35 +0800

    '不想叙述了'

--- js/play.js
+++ js/play.js
@@ -1,23 +1,23 @@
-window.onload = function(){
-var more = document.getElementsByClassName('more')[0];
-var more1 = document.getElementsByClassName('more1')[0];
-more.addEventListener('click',function(){
-more.style.display = 'none';
-more1.style.display = 'block';
-})
-var category  = document.getElementsByTagName('span')[2];
-var hottest = document.getElementsByTagName('span')[1];
-hottest.addEventListener('click',()=>{
-    window.location.href='./play02.html'
-})
+window.addEventListener('load',function(){
+        var more = document.getElementsByClassName('more')[0];
+        var more1 = document.getElementsByClassName('more1')[0];
+        more.addEventListener('click',function(){
+        more.style.display = 'none';
+        more1.style.display = 'block';
+        })
+        var category  = document.getElementsByTagName('span')[2];
+        var hottest = document.getElementsByTagName('span')[1];
+        hottest.addEventListener('click',()=>{
+            window.location.href='./play02.html'
+        })
 
-category.addEventListener('click',()=>{
-    window.location.href='./play03.html'
+        category.addEventListener('click',()=>{
+            window.location.href='./play03.html'
+        })
+            var li = document.getElementsByTagName('li');
+            for(var i=0;i<li.length;i++){
+            li[i].onclick = function(){
+                window.location.href = './playskip.html'
+            }
+        }
 })
-    var li = document.getElementsByTagName('li');
-    for(var i=0;i<li.length;i++){
-    li[i].onclick = function(){
-        window.location.href = './playskip.html'
-    }
-}
-}
