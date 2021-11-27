c118134ffde76e25000fbe464d09d5566fcca514
江冰封 <2944279298@qq.com>
2021-11-27 10:19:02 +0800

    'mark.js编写，作用于蒙版'

---
+++ js/mark.js
@@ -0,0 +1,18 @@
+window.addEventListener('load',function(){
+    var body = document.getElementsByTagName('body')[0];
+    var div_ = document.createElement('div');
+    div_.style.position = 'absolute';
+    div_.style.width = this.window.innerWidth+'px';
+    div_.style.height = this.window.innerHeight+'px';
+    div_.style.zIndex = '999';
+    var str = `<img src="../img/loading.gif" 
+                style="position:absolute;left:50%;top:50%;transform:translate(-50%,-50%);"/>`
+    div_.innerHTML = str;
+    div_.style.backgroundColor = 'white';
+    div_.style.left = 0;
+    div_.style.top = 0;
+    body.appendChild(div_);
+    this.setTimeout(function(){
+        body.removeChild(div_);
+    },800);
+})
