a65c0c7aa0bb09405adfe97b8d4f2da6b5229a15
Ares <1390566919@qq.com>
2021-11-27 11:37:31 +0800

    'wmnb'

--- js/mark.js
+++ js/mark.js
@@ -3,15 +3,16 @@ window.addEventListener('load', function () {
     var div_ = document.createElement('div');
     div_.style.position = 'absolute';
     div_.style.width = this.window.innerWidth + 'px';
-    div_.style.height = this.window.innerHeight + 'px';
+    div_.style.height = '2000px';
     div_.style.zIndex = '999';
     var str = `<img src="../img/loading.gif" 
-                style="position:absolute;left:50%;top:50%;transform:translate(-50%,-50%);"/>`
+                style="position:absolute;left:50%;top:200px;transform:translate(-50%,0);"/>`
     div_.innerHTML = str;
     div_.style.backgroundColor = 'white';
     div_.style.left = 0;
     div_.style.top = 0;
     body.appendChild(div_);
+
     this.setTimeout(function () {
         body.removeChild(div_);
     }, 800);
